df2961c86e2c3b16ead57eb4716fab708d51fab5
Mick <wang.mick12@gmail.com>
2025-01-09 22:56:48 -0500

    basic set up

---
+++ app/about/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">About Me</h1>
+      <p className="mb-4">
+        Hi, 
+        I am a graphic designer, creating mainly posters and novelties within organizations at the University of Waterloo.
+        I have been working within MathSoc as well as DSC and Lifesaving Club.
+        I'm testing the waters with character design, looking to creating unique characters for various media, including games, animations, and
+        comics.
+      </p>
+      <p>
+        Feel free to browse through my portfolio and don't hesitate to reach out
+        if you'd like to collaborate on a project!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
