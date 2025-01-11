c1c0ba2ba1f3fd529c8c8e8ddf21149fa0bc2f7c
Mick <wang.mick12@gmail.com>
2025-01-11 13:48:59 -0500

    update header with social media links

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,30 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { FaBehanceSquare, FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <header className="bg-gray-800 text-white">
-      <nav className="container mx-auto px-4 py-6">
-        <ul className="flex space-x-4">
-          <li><Link href="/" className="hover:text-gray-300">Home</Link></li>
-          <li><Link href="/about" className="hover:text-gray-300">About</Link></li>
-        </ul>
+    <header className="bg-gray-800 text-white px-6 py-4">
+      <nav className="flex items-center justify-between container mx-auto">
+        <div>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/" className="hover:text-gray-500">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="hover:text-gray-500">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex justify-right gap-4 text-3xl">
+          <Link href="https://www.behance.net/lazymango" target="_blank" className="hover:text-gray-500"><FaBehanceSquare /></Link>
+          <Link href="https://github.com/mwango-phoenix" target="_blank" className="hover:text-gray-500"><FaGithubSquare /></Link>
+          <Link href="https://linkedin.com/in/mwango-phoenix" target="_blank" className="hover:text-gray-500"><FaLinkedin /></Link>
+        </div>
       </nav>
     </header>
-  )
+  );
 }
-
